50e62a693fded555efb54dc5d487aab82ccb2474
Sagar Chougule <92chougulesagar@gmail.com>
2022-10-26 11:35:32 +0530

    add field

---
+++ 4_Add_Field.py
@@ -0,0 +1,7 @@
+#Code for Add Field
+#Developer: Sagar Chougule
+#Contact : 92chougulesagar@gmail.com, https://github.com/92sagarchougule
+
+import arcpy
+arcpy.AddField_management('Roads','Length',"FLOAT")
+
